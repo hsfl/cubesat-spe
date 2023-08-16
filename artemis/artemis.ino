1a70bcc9271dff4ed3aab6c8e99a24322e3c9c7d
montoyaoa <99383055+montoyaoa@users.noreply.github.com>
2023-08-15 14:20:58 -1000

    Basic OBC polling
    Artemis OBC board polls sensor board every 5 seconds for latest sensor readings. Received packets are copied into the receive buffer and copied into the sensors_event_t objects for printing whenever a new reading is received.

--- artemis/artemis.ino
+++ artemis/artemis.ino
@@ -1,18 +1,31 @@
 #include "SparkFun_SinglePairEthernet.h"
+// Adafruit Unified Sensor
+
+#define REQUEST 0x0A;
 
 SinglePairEthernet adin1110;
+
+unsigned long last_report;
 
 const int MAX_MSG_SIZE = 200;
 
 byte artemisMAC[6] = {0x00, 0xE0, 0x22, 0xFE, 0xDA, 0xC9};
 byte esp32MAC[6] = {0x00, 0xE0, 0x22, 0xFE, 0xDA, 0xCA};
 
+bool xmitFlag = false;
+bool recvFlag = false;
+
 byte xmitPacket[MAX_MSG_SIZE];
 byte recvPacket[MAX_MSG_SIZE];
+
+sensors_event_t accel;
+sensors_event_t gyro;
+sensors_event_t temp;
 
 static void rxCallback(byte * data, int dataLen, byte * senderMac)
 {
   memcpy(recvPacket, data, dataLen);
+  recvFlag = true;
 }
 
 void setup() {
@@ -38,16 +51,53 @@ void setup() {
 }
 
 void loop() {
-  /* If we are still connected, send a message */
-  if(adin1110.getLinkStatus())
-  {      
-      Serial.print("Sending:\t");
-      Serial.println(); //This is ok since we know they are all null terminated strings
-      
-      adin1110.sendData((byte *)xmitPacket, sizeof(xmitPacket), destinationMAC);
-  }
-  else
+  unsigned long now = millis();
+  
+  if(now-last_report >= 5000)
   {
+    if (adin1110.getLinkStatus())
+    {
+      Serial.println("Requesting data from sensor board...");
+      memset(xmitPacket, 0, sizeof(xmitPacket));
+      memcpy(xmitPacket, REQUEST, 1);
+      adin1110.sendData(xmitPacket, 1);
+      last_report = now;
+    }
+    else
+    {
       Serial.println("Waiting for link to resume sending");
+    }
+  }
+
+  if(recvFlag)
+  {
+    memcpy(accel, recvPacket, sizeof(accel));
+    memcpy(gyro, recvPacket + sizeof(accel), sizeof(gyro));
+    memcpy(temp, recvPacket + sizeof(accel) + sizeof(gyro), sizeof(temp));
+    
+    Serial.print("\t\tTemperature ");
+    Serial.print(temp.temperature);
+    Serial.println(" deg C");
+  
+    /* Display the results (acceleration is measured in m/s^2) */
+    Serial.print("\t\tAccel X: ");
+    Serial.print(accel.acceleration.x);
+    Serial.print(" \tY: ");
+    Serial.print(accel.acceleration.y);
+    Serial.print(" \tZ: ");
+    Serial.print(accel.acceleration.z);
+    Serial.println(" m/s^2 ");
+  
+    /* Display the results (rotation is measured in rad/s) */
+    Serial.print("\t\tGyro X: ");
+    Serial.print(gyro.gyro.x);
+    Serial.print(" \tY: ");
+    Serial.print(gyro.gyro.y);
+    Serial.print(" \tZ: ");
+    Serial.print(gyro.gyro.z);
+    Serial.println(" radians/s ");
+    Serial.println();
+
+    recvFlag = false;
   }
 }
